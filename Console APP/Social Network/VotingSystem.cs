51e6aa77d41447ef586721075c9f1f4ea821ba30
davidlubomirov <davidlubomirov@gmail.com>
2016-06-25 16:22:31 +0300

    Update

---
+++ Console APP/Social Network/VotingSystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Social_Network
+{
+    public struct VotingSystem
+    {
+        // Must implement event handler 
+    }
+}
